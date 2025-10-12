9cf602c52fb6c72863ff489332bb74e80fbe4e53
Soyeb Islam <soyeb.islam117@gmail.com>
2025-10-12 18:11:33 +0600

    basic server setup and connect to db

---
+++ src/server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Test route
+app.get("/", (req, res) => {
+    res.json({message: "TaskIQ API is running."});
+})
+
+
+// Start server and connect to DB
+const PORT = process.env.PORT || 5000;
+mongoose.connect(process.env.MONGO_URI)
+.then(() => app.listen(PORT, () => console.log(`Server is running on port ${PORT}`)
+))
+.catch((err) => console.error("DB connection error:", err)
+)
